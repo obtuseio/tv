d4a063421e7cad6d0f7ba6cc2d7225efe5f4ec31
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-01-04 16:48:20 +0530

    feat(back): read, parse, and collect partial episodes by id

--- back/src/main.rs
+++ back/src/main.rs
@@ -18,6 +18,15 @@ struct Rating {
 
 type RatingsById<'a> = BTreeMap<&'a str, Rating>;
 
+#[derive(Debug)]
+struct PartialEpisode<'a> {
+    series_id: &'a str,
+    season_number: Option<u32>,
+    episode_number: Option<u32>,
+}
+
+type PartialEpisodesById<'a> = BTreeMap<&'a str, PartialEpisode<'a>>;
+
 fn main() {
     let string = read("data/title.ratings.tsv");
     let ratings_by_id: RatingsById = string
@@ -32,6 +41,25 @@ fn main() {
                 Rating {
                     average: n().parse().unwrap(),
                     count: n().parse().unwrap(),
+                },
+            )
+        })
+        .collect();
+
+    let string = read("data/title.episode.tsv");
+    let partial_episodes_by_id: PartialEpisodesById = string
+        .trim()
+        .split('\n')
+        .skip(1)
+        .map(|line| {
+            let mut parts = line.trim().split('\t');
+            let mut n = || parts.next().unwrap();
+            (
+                n(),
+                PartialEpisode {
+                    series_id: n(),
+                    season_number: n().parse().ok(),
+                    episode_number: n().parse().ok(),
                 },
             )
         })
