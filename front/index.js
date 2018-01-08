df5496cd1c1e93f34389dc764bd77aa187273f0b
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-01-08 10:26:10 +0530

    feat(front): init

---
+++ front/index.js
@@ -0,0 +1,3 @@
+import Elm from './src/Main.elm';
+
+Elm.Main.embed(document.getElementById('app'));
---
+++ front/src/Main.elm
@@ -0,0 +1,8 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+main : Html msg
+main =
+    text "Hello, world!"
